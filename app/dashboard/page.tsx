774b9fa442f27628093ab61d5b25a35eb67c46b1
yassinezadod <yassine.zadod@e-polytechnique.ma>
2024-06-04 16:09:51 +0100

    secound commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,10 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
- 
+
 export default async function Page() {
   const revenue = await fetchRevenue();
-  // ...
+  return <div>
+    <RevenueChart revenue={revenue} />
+  </div>;
 }
